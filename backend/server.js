963146231bfe19c6a12eaa441875d83744c9d0d0
Honey <honey.bhardwaj@oriserve.com>
2025-07-21 15:00:17 +0530

    deployment dependencies added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
 const app = require("./app");
+const cors = require("cors");
+app.use(cors({
+  origin: process.env.FRONTEND_URL, // Add frontend URL later
+  credentials: true
+}));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
